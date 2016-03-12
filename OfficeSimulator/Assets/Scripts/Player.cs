d79b3bdf5eaa08546cff396db9ecd6210528b8fa
nertro <schneenerdchen@googlemail.com>
2016-03-13 00:34:40 +0100

    ADDED DoTask Function

--- OfficeSimulator/Assets/Scripts/Player.cs
+++ OfficeSimulator/Assets/Scripts/Player.cs
@@ -3,13 +3,40 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    private int reputation;
+    private int mood;
+    [SerializeField]
+    private int moodtweak; //for workspeed calculation
+    private float task;
+    private bool doingTask;
+    private bool watchingCats;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Update()
+    {
+        if (doingTask)
+        {
+            DoTask();
+        }
+        else if (watchingCats)
+        {
+            
+        }
+    }
+
+    private void DoTask()
+    {
+        if (task < 100)
+        {
+            float workspeed = (mood / moodtweak) * Time.deltaTime;
+            task += workspeed;
+            Mathf.Clamp(task, 0, 100);
+        }
+    }
+
 }
