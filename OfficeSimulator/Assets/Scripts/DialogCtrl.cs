c94c63b0c31d0a41588c8ba62908cf6aa22d85e9
nertro <schneenerdchen@googlemail.com>
2016-03-13 19:10:20 +0100

    ADDED high Confrontation List

--- OfficeSimulator/Assets/Scripts/DialogCtrl.cs
+++ OfficeSimulator/Assets/Scripts/DialogCtrl.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml;
@@ -7,17 +9,39 @@ using System.IO;
 public class DialogCtrl : MonoBehaviour {
 
     private List<Confrontation> lowConfrontations;
+    private List<Confrontation> highConfrontation;
+
+    [SerializeField]
+    private Text dialogueText;
+    [SerializeField]
+    private Button[] answers;
 
     void Awake()
     {
         lowConfrontations = new List<Confrontation>();
         LoadConfrontations();
     }
+
+    void Start()
+    {
+        GameCtrl.DialogueEvent += OnDialogue;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    private void OnDialogue(object sender, CustomEventArgs e)
+    {
+
+    }
+
+    private void ChooseConfrontation(float reputation)
+    {
+        float chance = 100 - reputation;
+        chance /= 100;
+    }
 
     private void LoadConfrontations()
     {
@@ -26,33 +50,59 @@ public class DialogCtrl : MonoBehaviour {
 
         while (reader.Read())
         {
+            reader.ReadToFollowing("Level1");
+
+
             if (reader.ReadToFollowing("Confrontation"))
             {
-                Confrontation temp = new Confrontation();
+                GetConfrontation(reader, lowConfrontations);
 
-                reader.ReadToFollowing("Question");
-                reader.ReadStartElement();
-                temp.Question = reader.ReadContentAsString();
-                lowConfrontations.Add(temp);
-
-                while (reader.ReadToNextSibling("Answer"))
+                while (reader.ReadToNextSibling("Confrontation"))
                 {
-                    string answer;
-                    int cost;
-                    reader.ReadStartElement();
-                    answer = reader.ReadContentAsString();
-                    reader.ReadToFollowing("Cost");
-                    reader.ReadStartElement();
-                    cost = reader.ReadContentAsInt();
-
-                    temp.Answers.Add(answer, cost);
+                    GetConfrontation(reader, lowConfrontations);
                 }
+            }
 
-                Debug.Log(temp.Question);
+            if (reader.ReadToFollowing("Level2"))
+            {
+                if (reader.ReadToFollowing("Confrontation"))
+                {
+                    GetConfrontation(reader, lowConfrontations);
+
+                    while (reader.ReadToNextSibling("Confrontation"))
+                    {
+                        GetConfrontation(reader, lowConfrontations);
+                    }
+                }
             }
         }
 
 
         stream.Close();
+    }
+
+    private void GetConfrontation(XmlReader reader, List<Confrontation> list)
+    {
+        Confrontation temp = new Confrontation();
+
+        reader.ReadToFollowing("Question");
+        reader.ReadStartElement();
+        temp.Question = reader.ReadContentAsString();
+
+        while (reader.ReadToNextSibling("Answer"))
+        {
+            string answer;
+            int cost;
+            reader.ReadStartElement();
+            answer = reader.ReadContentAsString();
+            reader.ReadToFollowing("Cost");
+            reader.ReadStartElement();
+            cost = reader.ReadContentAsInt();
+
+            temp.Answers.Add(answer, cost);
+        }
+
+        list.Add(temp);
+        Debug.Log(temp.Question);
     }
 }
