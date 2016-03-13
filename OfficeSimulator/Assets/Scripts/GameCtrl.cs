99e443c4d0e81aa2e91ac51076fa77c463bfebe0
nertro <schneenerdchen@googlemail.com>
2016-03-13 16:50:18 +0100

    ADDED Dialogue event to GameCtrl

---
+++ OfficeSimulator/Assets/Scripts/GameCtrl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class GameCtrl : MonoBehaviour {
+    [SerializeField]
+    private DialogCtrl dialogCtrl;
+
+    public static event EventHandler DialogueEvent;
+
+	void Start () {
+        dialogCtrl.gameObject.SetActive(false);
+	}
+	
+	void Update () {
+	
+	}
+
+    private void OnDialogue()
+    {
+        if (DialogueEvent == null)
+            return;
+
+        DialogueEvent(this, new EventArgs());
+    }
+}
